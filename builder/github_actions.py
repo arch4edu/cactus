4995473ee4e8bae2883cf1e76f5f0f2379b0a86a
Jingbei Li <i@jingbei.li>
2022-06-26 23:43:05 +0800

    added self hosted x86_64

--- builder/github_actions.py
+++ builder/github_actions.py
@@ -7,5 +7,7 @@ g = Github(config['github']['token'])
 def build(pkgbase, group):
     if group == 'GitHubActions':
         g.get_repo(config['github']['cactus']).get_workflow("builder_github_actions.yml").create_dispatch('main', {'pkgbase': pkgbase})
+    elif group == 'x86_64':
+        g.get_repo(config['github']['cactus']).get_workflow("builder_self_hosted_x86_64.yml").create_dispatch('main', {'pkgbase': pkgbase})
     elif group == 'aarch64':
         g.get_repo(config['github']['cactus']).get_workflow("builder_self_hosted_aarch64.yml").create_dispatch('main', {'pkgbase': pkgbase})
