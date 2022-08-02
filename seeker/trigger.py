e1b3fb4fe26d089d55939d2ce0e903a3564c71e9
Jingbei Li <i@jingbei.li>
2022-08-02 14:45:37 +0800

    trigger rebuild also empty oldver

--- seeker/trigger.py
+++ seeker/trigger.py
@@ -2,11 +2,18 @@
 
 if __name__ == '__main__':
     import sys
-    from ..models import Status
+    from ..models import Status, Version
+    from .. import logger
 
     pkgbase = sys.argv[1]
-    print(f'Marking {pkgbase} as staled')
+
     status = Status.objects.get(key=pkgbase)
-    if not status.status == 'STALED':
+    if status.status != 'STALED':
         status.status = 'STALED'
         status.save()
+    logger.info(f'Marked {pkgbase} as staled.')
+
+    for record in Version.objects.filter(key__startswith=pkgbase):
+        record.oldver = ''
+        record.save()
+        logger.info(f'Removed oldver from {record.key}')
