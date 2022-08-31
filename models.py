0aab04594c72e3f253a2ed577f5b5d425dc0d740
Jingbei Li <i@jingbei.li>
2022-08-31 17:46:36 +0800

    increased key length

--- models.py
+++ models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Version(models.Model):
-    key = models.CharField(max_length=50, default="", primary_key=True)
+    key = models.CharField(max_length=100, default="", primary_key=True)
     oldver = models.CharField(max_length=50, default="")
     newver = models.CharField(max_length=50, default="")
     timestamp = models.DateTimeField(auto_now=True)
 
 class Status(models.Model):
-    key = models.CharField(max_length=50, default="", primary_key=True)
+    key = models.CharField(max_length=100, default="", primary_key=True)
     status = models.CharField(max_length=30, default="")
     detail = models.CharField(max_length=200, default="")
     workflow = models.CharField(max_length=20, default="")
@@ -15,7 +15,7 @@ class Status(models.Model):
 
 class Package(models.Model):
     id = models.AutoField(primary_key=True)
-    key = models.CharField(max_length=50, default="")
+    key = models.CharField(max_length=100, default="")
     package = models.CharField(max_length=100, default="")
     age = models.PositiveSmallIntegerField(default=0)
     timestamp = models.DateTimeField(auto_now_add=True)
