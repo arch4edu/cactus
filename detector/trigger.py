1278379666ad35d8c5e92e9d199d712731d2f971
Jingbei Li <i@jingbei.li>
2022-11-02 18:20:31 +0800

    trigger rebuild for multiple packages

--- detector/trigger.py
+++ detector/trigger.py
@@ -5,15 +5,14 @@ if __name__ == '__main__':
     from ..models import Status, Version
     from .. import logger
 
-    pkgbase = sys.argv[1]
+    for pkgbase in sys.argv[1:]:
+        status = Status.objects.get(key=pkgbase)
+        if status.status != 'STALED':
+            status.status = 'STALED'
+            status.save()
+        logger.info(f'Marked {pkgbase} as staled.')
 
-    status = Status.objects.get(key=pkgbase)
-    if status.status != 'STALED':
-        status.status = 'STALED'
-        status.save()
-    logger.info(f'Marked {pkgbase} as staled.')
-
-    for record in Version.objects.filter(key__startswith=pkgbase):
-        record.oldver = ''
-        record.save()
-        logger.info(f'Removed oldver from {record.key}')
+        for record in Version.objects.filter(key__startswith=pkgbase):
+            record.oldver = ''
+            record.save()
+            logger.info(f'Removed oldver from {record.key}')
